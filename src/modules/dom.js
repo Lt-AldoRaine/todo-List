10c83f0900131939e956eab08161ff7d7016e141
Aldo Raine <105580292+Lt-AldoRaine@users.noreply.github.com>
2023-08-02 01:12:00 -0500

    Add ability to select projects and display tasks for that project

--- src/modules/dom.js
+++ src/modules/dom.js
@@ -1,59 +1,45 @@
-import taskDisplay from "./taskDisplay";
-import projectDisplay from "./projectDisplay";
+import { projectDisplay, displayTasks } from "./projectDisplay";
+import { saveProjects, getProjects } from "./storage";
 import Task from "./task";
 import Project from "./project";
-import store from "./storage";
 
 export default function dom() {
   const taskContainer = document.getElementById("task-wrapper");
   const taskFormContainer = document.getElementById("task-form-wrapper");
-  const projectSelect = document.getElementById("project-select");
   const projectForm = document.getElementById("project-form");
+  const projectList = document.getElementById("project-list");
+  const projectInput = document.getElementById("project-input");
+  const projectWrappers = document.querySelectorAll("#project-list .projects");
   const taskForm = document.getElementById("task-form");
-  const projects = document.querySelectorAll("project");
   const createTaskBtn = document.getElementById("create-task");
   const addTaskBtn = document.getElementById("task-submit");
   const createProjectBtn = document.getElementById("create-project");
   const addProjectBtn = document.getElementById("add-project");
   const deleteTaskBtn = document.querySelectorAll("delete-task");
 
-  const addProjectSelect = (projectTitle) => {
-    const option = document.createElement('option')
+  let projects = getProjects();
+  let currentProject = projects[0];
 
-    option.value = projectTitle
-    option.text = projectTitle
-    
-    console.log(option)
-    projectSelect.appendChild(option)
+  const createProject = () => {
+    const name = projectInput.value;
+    return new Project(name);
   };
 
-  const addTask = (e) => {
-    e.preventDefault();
-    const taskTitle = document.getElementById("task-input").value;
-    const dueDate = document.getElementById("task-date").value;
+  const createTask = () => {
+    const name = document.getElementById("task-input").value;
+    const date = document.getElementById("task-date").value;
     const priority = document.getElementById("task-priority").value;
-    const project = document.getElementById("project-select").value;
-    
-    taskForm.reset();
-    return new Task(taskTitle, dueDate, priority, project);
-  };
 
-  const addProject = (e) => {
-    e.preventDefault();
-    const projectTitle = document.getElementById("project-input").value;
-
-    addProjectSelect(projectTitle);
-    projectForm.reset();
-    return new Project(projectTitle);
+    return new Task(name, date, priority);
   };
 
   const displayProjectForm = () => {
-    document.querySelector("form").reset();
+    projectForm.reset();
     projectForm.setAttribute("style", "display: flex");
   };
 
   const displayTaskForm = () => {
-    document.querySelector("form").reset();
+    taskForm.reset();
     taskContainer.setAttribute("style", "display: none");
     taskFormContainer.setAttribute("style", "display: flex");
   };
@@ -63,18 +49,83 @@ export default function dom() {
     taskFormContainer.setAttribute("style", "display: none");
   };
 
-  createProjectBtn.onclick = displayProjectForm;
-  addProjectBtn.onclick = (e) => {
-    const project = addProject(e);
-    projectDisplay(project);
-    projectForm.setAttribute("style", "display: none");
+  const validProject = () => {
+    const project = document.querySelector("#project-input")
+
+    if (!project.value) {
+      project.setCustomValidity("Give project a name");
+      project.reportValidity();
+      return false
+    }
+
+    return true
+  };
+
+  const validTask = () => {
+    const task = document.querySelector("#task-input")
+
+    if (!task.value) {
+      task.setCustomValidity("Give task a name");
+      task.reportValidity();
+      return false
+    }
+
+    return true
+  };
+
+  const addProject = (e) => {
+    e.preventDefault();
+    const newProj = createProject();
+
+    currentProject = newProj;
+    projects.push(newProj);
+
+    saveProjects(projects);
+    projectDisplay(projects);
+  };
+
+  const addTask = (e, task) => {
+    e.preventDefault();
+    if (currentProject) {
+      console.log(currentProject);
+      currentProject.addTodo(task);
+
+      displayTasks(currentProject);
+      saveProjects(projects);
+      displayTaskList();
+    }
   };
 
   createTaskBtn.onclick = displayTaskForm;
-  addTaskBtn.onclick = (e) => {
-    const task = addTask(e);
-    console.log(task);
-    taskDisplay(task);
-    displayTaskList();
-  };
+  createProjectBtn.onclick = displayProjectForm;
+  projectList.addEventListener("click", (e) => {
+    if (e.target.classList.contains("project")) {
+      const projId = e.target.getAttribute("data-id");
+      currentProject = projects[projId];
+    }
+    displayTasks(currentProject);
+    console.log(currentProject);
+  });
+  addProjectBtn.addEventListener("click", (e) => {
+    if (validProject()) {
+      addProject(e);
+      projectForm.setAttribute("style", "display: none");
+    }
+  });
+  addTaskBtn.addEventListener("click", (e) => {
+    const task = createTask();
+    if (validTask()) {
+      addTask(e, task);
+      taskForm.setAttribute("style", "display: none");
+    }
+  });
+
+  document.addEventListener("DOMContentLoaded", () => {
+    if (projects === 0) {
+      projects = getProjects();
+    }
+    projectDisplay(projects);
+    displayTasks(currentProject);
+    console.log(currentProject);
+  });
 }
